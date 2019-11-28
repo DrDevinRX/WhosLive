98087e9c10e9ece88ad1a224d83512ecb6336dbf
Devin <DevinGoldstein@Knights.ucf.edu>
2019-11-27 23:20:47 -0500

    Add project files.

---
+++ WhosLive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhosLive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
